01901f1a71ef9d7e180297a60c8484fced81ddca
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-06-30 16:38:17 +0800

    fixing error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,8 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/sentinelsrobotics/sentinelsrobotics.github.io/edit/main",
+            
           routeBasePath: "/",
         },
         blog: false,
