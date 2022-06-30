d3286f96643b8dc5da00a5b5eb4e899fa98f4560
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-06-30 16:32:13 +0800

    github workflows

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-
+  trailingSlash:false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "sentinelsrobotics", // Usually your GitHub org/user name.
